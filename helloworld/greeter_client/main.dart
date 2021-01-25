e121429cf87763df621fbe550c28896850ca8348
Eng Zer Jun <engzerjun@gmail.com>
2021-01-25 13:18:47 +0800

    feat: add Dart client
    Signed-off-by: Eng Zer Jun <engzerjun@gmail.com>

---
+++ helloworld/greeter_client/main.dart
@@ -0,0 +1,26 @@
+import 'package:grpc/grpc.dart';
+
+import 'package:helloworld/src/generated/helloworld.pb.dart';
+import 'package:helloworld/src/generated/helloworld.pbgrpc.dart';
+
+Future<void> main(List<String> args) async {
+  final channel = ClientChannel(
+    'localhost',
+    port: 50051,
+    options: const ChannelOptions(credentials: ChannelCredentials.insecure()),
+  );
+  final stub = GreeterClient(channel);
+
+  final name = args.isNotEmpty ? args[0] : 'world';
+
+  try {
+    final response = await stub.sayHello(HelloRequest()..name = name);
+    print('Greeter client received: ${response.message}');
+
+    final responseAgain = await stub.sayHelloAgain(HelloRequest()..name = name);
+    print('Greeter client received again: ${responseAgain.message}');
+  } catch (e) {
+    print('Caught error: $e');
+  }
+  await channel.shutdown();
+}
